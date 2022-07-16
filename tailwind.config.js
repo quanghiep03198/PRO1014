94beb94508d850488734c117630194fcb5bbf565
quanghiep031 <quanghiep03198@gmail.com>
2022-07-16 15:16:08 +0700

    config: reponsive breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
 	content: ["./index.html", "./index.php"],
 	theme: {
 		extend: {},
+		screens: {
+			sm: "375px",
+
+			md: { min: "600px", max: "1365px" },
+
+			lg: "1366px",
+		},
 	},
 	plugins: [require("daisyui")],
 };
