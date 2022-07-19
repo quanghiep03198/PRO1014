7dd957a266c99647f1d04c71916bb1535e0a9b63
quanghiep031 <quanghiep03198@gmail.com>
2022-07-19 23:46:28 +0700

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./index.html", "./index.php"],
+	content: ["./index.html", "./index.php", "./admin.php"],
 	theme: {
 		extend: {},
 		screens: {
