12f13f029b33fff3bb31dddadfbd23b198dc4c5b
quanghiep031 <quanghiep03198@gmail.com>
2022-07-20 18:30:22 +0700

    css

--- admin/components/sidebar.php
+++ admin/components/sidebar.php
@@ -1,26 +1,26 @@
 <div class="fixed h-[100vh] dropdown dropdown-right">
     <!-- nav-icon -->
-    <nav class="h-100 p-[30px] flex flex-col bg-[color:var(--primary-color)] h-[100vh] justify-between items-center" tabindex="1">
-        <ul class="flex flex-col justify-between items-center min-h-[20rem]">
-            <li class="text-[#fff] text-2xl hover:scale-105">
+    <nav class="h-100 p-[30px] flex flex-col justify-between items-center bg-[color:var(--primary-color)] h-[100vh] " tabindex="0">
+        <ul class="flex flex-col justify-center gap-4 items-center h-1/2">
+            <li class=" text-[#fff] text-2xl hover:scale-105">
                 <i class="bi bi-bar-chart-line "></i>
             </li>
-            <li class="text-[#fff] text-2xl hover:scale-105">
+            <li class=" text-[#fff] text-2xl hover:scale-105">
                 <i class="bi bi-people"></i>
             </li>
-            <li class="text-[#fff] text-2xl hover:scale-105">
+            <li class=" text-[#fff] text-2xl hover:scale-105">
                 <i class="bi bi-box"></i>
             </li>
-            <li class="text-[#fff] text-2xl hover:scale-105">
+            <li class=" text-[#fff] text-2xl hover:scale-105">
                 <i class="bi bi-card-checklist"></i>
             </li>
-            <li class="text-[#fff] text-2xl hover:scale-105">
+            <li class=" text-[#fff] text-2xl hover:scale-105">
                 <i class="bi bi-newspaper"></i>
             </li>
-            <li class="text-[#fff] text-2xl hover:scale-105">
+            <li class=" text-[#fff] text-2xl hover:scale-105">
                 <i class="bi bi-list-check"></i>
             </li>
-            <li class="text-[#fff] text-2xl hover:scale-105">
+            <li class=" text-[#fff] text-2xl hover:scale-105">
                 <i class="bi bi-gear"></i>
             </li>
         </ul>
@@ -29,19 +29,19 @@
         </div>
     </nav>
     <!-- submenu -->
-    <nav class="flex flex-col justify-between h-[100vh] bg-[#858585] px-2 py-[30px] dropdown-content w-[340px]" tabindex="0">
-        <ul class="flex flex-col justify-between min-h-[20.5rem]">
-            <li class="w-full p-2 text-white text-md hover:bg-white/30 hover:rounded-lg"><a href="#">Thống kê doanh số</a> </li>
-            <li class="w-full p-2 text-white text-md hover:bg-white/30 hover:rounded-lg"><a href="#">Quản lý tài khoản</a> </li>
-            <li class="w-full p-2 text-white text-md hover:bg-white/30 hover:rounded-lg"><a href="#">Quản lý sản phẩm</a> </li>
-            <li class="w-full p-2 text-white text-md hover:bg-white/30 hover:rounded-lg"><a href="#">Quản lý đơn hàng</a> </li>
-            <li class="w-full p-2 text-white text-md hover:bg-white/30 hover:rounded-lg"><a href="#">Quản lý bài viết</a></li>
-            <li class="w-full p-2 text-white text-md hover:bg-white/30 hover:rounded-lg"><a href="#">Danh sách dịch vụ</a> </li>
-            <li class="w-full p-2 text-white text-md hover:bg-white/30 hover:rounded-lg"><a href="#">Cài đặt website</a> </li>
+    <nav class="dropdown-content flex flex-col justify-between w-96 h-[100vh] bg-[#858585] px-2 py-[30px]" tabindex="0">
+        <ul class="flex flex-col justify-center gap-2 h-1/2">
+            <li class="w-full p-2 text-white text-base hover:bg-white/30 hover:rounded-lg"><a href="#">Thống kê doanh số</a> </li>
+            <li class="w-full p-2 text-white text-base hover:bg-white/30 hover:rounded-lg"><a href="#">Quản lý tài khoản</a> </li>
+            <li class="w-full p-2 text-white text-base hover:bg-white/30 hover:rounded-lg"><a href="#">Quản lý sản phẩm</a> </li>
+            <li class="w-full p-2 text-white text-base hover:bg-white/30 hover:rounded-lg"><a href="#">Quản lý đơn hàng</a> </li>
+            <li class="w-full p-2 text-white text-base hover:bg-white/30 hover:rounded-lg"><a href="#">Quản lý bài viết</a></li>
+            <li class="w-full p-2 text-white text-base hover:bg-white/30 hover:rounded-lg"><a href="#">Danh sách dịch vụ</a> </li>
+            <li class="w-full p-2 text-white text-base hover:bg-white/30 hover:rounded-lg"><a href="#">Cài đặt website</a> </li>
         </ul>
-        <div class="flex flex-col justify-center gap-1 align-middle h-[60px]">
-            <span class="text-white text-xl">Adminstrator</span>
-            <span class="text-gray-300">admin@gmail.com</span>
+        <div class="flex flex-col justify-center align-middle h-[60px]">
+            <span class="px-2 text-white text-xl">Adminstrator</span>
+            <span class="px-2 text-gray-300">admin@gmail.com</span>
         </div>
     </nav>
 </div>
