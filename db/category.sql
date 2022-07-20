369a17f12a89adf3dba340c24d541a29f5993f32
quanghiep031 <quanghiep03198@gmail.com>
2022-07-21 00:25:49 +0700

    sql: CRUD product_category

--- db/category.sql
+++ db/category.sql
@@ -2,10 +2,10 @@
 SELECT * FROM * product_category;
 
 -- thêm danh mục sản phẩm
-INSERT INTO product_category (?) VALUES(?);
+INSERT INTO product_category (cate_name, cate_image) VALUES(?,?);
 
 -- sửa danh mục sản phẩm
-UPDATE product_category SET ? = > WHERE cate_id = ?; 
+UPDATE product_category SET cate_name = ? , cate_image = ? WHERE cate_id = ?; 
 
 -- xóa danh mục sản phẩm
 DELETE FROM product_category 
