78bf71d9b792df635fa54f49b3141f1628280e9c
Nguyen-Vinh-Quang <nvq.ndcg@gmail.com>
2022-07-20 21:21:43 +0700

    insert category

--- db/category.sql
+++ db/category.sql
@@ -1,8 +1,12 @@
 -- lấy danh sách danh mục sản phẩm
+SELECT * FROM * product_category;
 
 -- thêm danh mục sản phẩm
-INSERT INTO category (cate_name) VALUES(`Máy Chơi Game`);
+INSERT INTO product_category (?) VALUES(?);
 
 -- sửa danh mục sản phẩm
+UPDATE product_category SET ? = > WHERE cate_id = ?; 
 
 -- xóa danh mục sản phẩm
+DELETE FROM product_category 
+WHERE cate_id = ?;
