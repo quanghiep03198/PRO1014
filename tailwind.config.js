f51097af679622f48b1f9c7a3e882b867c5c3570
quanghiep031 <quanghiep03198@gmail.com>
2022-07-21 23:03:57 +0700

    feat: just in time mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	content: ["site/**/*.{html,js,php}", "admin/**/*.{html,js,php}"],
+	mode: "jit",
+	purge: ["site/**/*.{html,js,php}", "admin/**/*.{html,js,php}"],
 	theme: {
 		extend: {},
 		screens: {
