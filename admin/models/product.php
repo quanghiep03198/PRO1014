1b229955b2b8d77a1f28ec6f21a33d1778b1ffe1
quanghiep031 <quanghiep03198@gmail.com>
2022-07-21 10:00:59 +0700

    feat: global function

--- admin/models/product.php
+++ admin/models/product.php
@@ -7,3 +7,32 @@ function get_all_products()
     $products = select_all_records($sql);
     return $products;
 }
+// lấy ra 1 sản phẩm
+function get_one_product()
+{
+    if (isset($_GET['id'])) :
+        $sql = "SELECT * FROM product WHERE product_id = ";
+        $product = select_single_record($sql);
+        return $product;
+    endif;
+}
+// thêm sản phẩm
+function add_product()
+{
+    global $error;
+    if (isset($_POST['add_product'])) :
+        if (!empty($error)) 
+        return;
+        $product_name = $_POST['product_name'];
+        $cate = $_POST['cate'];
+        $price = $_POST['price'];
+        $description = $_POST['description'];
+        $stock= $_POST['stock'];
+        $warranty_time = $_POST['warranty'];
+        $manufacturer = $_POST['manu'];
+$img = 
+        $sql = "INSERT INTO product (product_name, cate_id, product_img, price, description, discount, stock, warranty_time, manu_id) 
+VALUES ();"
+        
+    endif;
+}
