6886e514f4810b461b713877a83bd61980c0f639
quanghiep031 <quanghiep03198@gmail.com>
2022-07-22 13:04:48 +0700

    config: bind selector function

--- site/components/header.php
+++ site/components/header.php
@@ -1,42 +1,49 @@
-    <header>
-        <div class="container">
-            <div class="navbar bg-base-100">
-                <div class="navbar-start">
-
-                    <a href=""><img class="max-h-[150px] max-w-[150px] hover:scale-[1.2]" src="/img/logo.png" alt=""></a>
-                </div>
-                <div class="navbar-center hidden lg:flex">
-                    <ul class="menu menu-horizontal p-0">
-                        <li><a href="" class="text-[20px] font-[600]">Trang chủ</a></li>
-                        <li><a href="" class="text-[20px] font-[600]">Cửa hàng</a></li>
-                        <li><a href="" class="text-[20px] font-[600]">Dịch vụ</a></li>
-                        <li><a href="" class="text-[20px] font-[600]">Tin tức</a></li>
-                    </ul>
-                </div>
-                <div class="navbar-end gap-[25px] pr-[10px]">
-                    <a href=""><img class="max-h-[25px] max-w-[25px] hover:scale-[1.5]" src="/img/Vector (1).png" alt=""></a>
-                    <a href=""><img class="max-h-[25px] max-w-[25px] hover:scale-[1.5]" src="/img/Heart.png" alt=""></a>
-                    <div class="indicator">
-                        <a href=""><img class="max-h-[25px] max-w-[25px] hover:scale-[1.5]" src="/img/Vector.png" alt=""></a>
-                        <span class="badge badge-sm badge-error indicator-item"></span>
+<header>
+    <div class="navbar justify-between bg-base-100">
+        <!-- logo -->
+        <div class="navbar-start">
+            <a href=""><img class="sm:max-w-[100px] lg:max-w-[150px]" src=<?= $ROOT_SITE . 'logo.png' ?> alt=""></a>
+        </div>
+        <!-- nav-link  -->
+        <div class="navbar-center hidden lg:flex">
+            <ul class="flex justify-center items-center gap-8">
+                <li class="h-10 text-[20px] font-[600] hover:border-b-4 hover:border-gray-800"><a href="?page=home">Trang chủ</a></li>
+                <li class="h-10 text-[20px] font-[600] hover:border-b-4 hover:border-gray-800"><a href="?page=product">Cửa hàng</a></li>
+                <li class="h-10 text-[20px] font-[600] hover:border-b-4 hover:border-gray-800"><a href="?page=service">Dịch vụ</a></li>
+                <li class="h-10 text-[20px] font-[600] hover:border-b-4 hover:border-gray-800"><a href="?page=news">Tin tức</a></li>
+            </ul>
+        </div>
+        <!-- user control -->
+        <div class="navbar-end gap-4 pr-[10px]">
+            <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img src="https://placeimg.com/80/80/people" />
                     </div>
-                    <div class="dropdown dropdown-end">
-                        <label tabindex="0" class="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
-                        </label>
-                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a href="">Trang chủ</a></li>
-                            <li><a href="">Cửa hàng</a></li>
-                            <li><a href="">Dịch vụ</a></li>
-                            <li><a href="">Tin tức</a></li>
-
-                        </ul>
-                    </div>
-                </div>
-
+                </label>
+                <!-- submenu -->
+                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-[300px]">
+                    <li>
+                        <a class="justify-between">Tài khoản <span class="badge badge-lg">Quang Hiệp</span></a>
+                    </li>
+                    <li><a href="?page=account-infor">Settings</a></li>
+                    <li><a href="../logout.php">Đăng xuất</a></li>
+                </ul>
+            </div>
+            <a href="?page=wishlist" class="text-xl"><i class="bi bi-heart"></i></a>
+            <div class="indicator">
+                <a href="?page=cart" class="text-xl"><i class="bi bi-cart3"></i></a>
+                <span class="badge badge-md badge-error indicator-item text-white">0</span>
+            </div>
+            <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-sm lg:hidden"><i class="bi bi-list"></i></label>
+                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a href="?page=home">Trang chủ</a></li>
+                    <li><a href="?page=product">Cửa hàng</a></li>
+                    <li><a href="?page=service">Dịch vụ</a></li>
+                    <li><a href="?page=news">Tin tức</a></li>
+                </ul>
             </div>
         </div>
-
-    </header>
+    </div>
+</header>
