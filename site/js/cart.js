e03f31ab59be116364cd57666c77a4508d4fb0d1
quanghiep031 <quanghiep03198@gmail.com>
2022-07-24 04:34:28 +0700

    feat: responsive product slider

--- site/js/cart.js
+++ site/js/cart.js
@@ -1,5 +1,6 @@
-import { $, $$ } from "./config.js";
-
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+localStorage.setItem("cart", JSON.stringify([]));
 // counter button
 const decrementButtons = $$(`button[data-action="decrement"]`);
 const incrementButtons = $$(`button[data-action="increment"]`);
@@ -22,9 +23,37 @@ incrementButtons.forEach((btn) => {
 		target.value = value;
 	});
 });
-
+/**
+ * Thêm sản phẩm vào giỏ hàng
+ */
+let cartItems = JSON.parse(localStorage.getItem("cart"));
+console.log(cartItems);
+function addCart(button) {
+	const product = {
+		id: button.parentElement.querySelector(`input[name = "id"]`).value,
+		name: button.parentElement.querySelector(`input[name = "name"]`).value,
+		img: button.parentElement.querySelector(`input[name = "img"]`).value,
+		price: +button.parentElement.querySelector(`input[name = "price"]`).value,
+		qty: +button.parentElement.querySelector(`input[name = "qty"]`).value,
+	};
+	// kiểm tra sản phẩm đã có trong giỏ hàng chưa? nếu đã tồn tại => update lại số lượng
+	const duplicatedItem = cartItems.find((item) => item.id == product.id);
+	if (duplicatedItem) {
+		duplicatedItem["qty"] += product.qty;
+		// alert(`Sản phẩm đã tồn tại trong giỏ hàng!`);
+	} else cartItems.push(product);
+	localStorage.setItem("cart", JSON.stringify(cartItems));
+	console.log(JSON.parse(localStorage.getItem("cart")));
+}
 // render cart item
-const renderCart = (cartItems) => {};
+const renderCart = (data) => {
+	return data
+		.map(
+			(item) => /*html */ `
+	`,
+		)
+		.join("");
+};
 // update cart item
 const name = (params) => {};
 // delete cart item
