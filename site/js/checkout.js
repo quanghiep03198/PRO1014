1f67549cac5c8170d88be77f4c306a1a75b07177
quanghiep031 <quanghiep03198@gmail.com>
2022-07-26 01:32:53 +0700

    FEAT: CHECKOUT

---
+++ site/js/checkout.js
@@ -0,0 +1,31 @@
+const $ = (selector) => {
+	const elements = document.querySelectorAll(selector);
+	return elements.length == 1 ? elements[0] : elements;
+};
+const form = $("#check-out__form");
+console.log(form);
+if (form) {
+	form.onsubmit = (event) => {
+		const customerName = form.querySelector(`input[name="customer_name"]`);
+		const phone = form.querySelector(`input[name="phone"]`);
+		const email = form.querySelector(`input[name="email"]`);
+		const address = form.querySelector(`input[name="address"]`);
+		const shipping = form.querySelector(`select[name="shipping_method"]`);
+		const payment = form.querySelector(`select[name="payment_method"]`);
+		const totalAmount = form.querySelector(`input[name="total_amount"]`);
+		const isNotEmpty = isRequired(customerName, phone, email, address, shipping, payment);
+		if (!isNotEmpty) event.preventDefault();
+		if (!isEmail(email)) event.preventDefault();
+		if (!isPhoneNumber(phone)) event.preventDefault();
+		if (isNotEmpty && isEmail(email) && isPhoneNumber(phone)) {
+			document.cookie = `cart=${localStorage.getItem("cart")}`;
+			const cartItems = JSON.parse(localStorage.getItem("cart"));
+			totalAmount.value =
+				+cartItems.reduce((previousValue, currentValue) => {
+					return previousValue + currentValue.total;
+				}, 0) + +shipping.value;
+			console.log(totalAmount.value);
+			// localStorage.setItem("cart", JSON.stringify([]));
+		}
+	};
+}
