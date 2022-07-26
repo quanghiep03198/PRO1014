cc57d008aec6ca5dbd1a4f0e0921c5259b31c5f8
Nguyen-Vinh-Quang <nvq.ndcg@gmail.com>
2022-07-26 12:21:57 +0700

    update category

--- db/category.sql
+++ db/category.sql
@@ -2,11 +2,14 @@
 SELECT * FROM * product_category;
 
 -- thêm danh mục sản phẩm
-INSERT INTO product_category (cate_name, cate_image) VALUES(?,?);
+INSERT INTO product_category (name,cate) VALUES(?,?);
 
 -- sửa danh mục sản phẩm
-UPDATE product_category SET cate_name = ? , cate_image = ? WHERE cate_id = ?; 
+UPDATE product_category SET 
+name = ? 
+image = ?
+WHERE cate_id = ?; 
 
 -- xóa danh mục sản phẩm
 DELETE FROM product_category 
-WHERE cate_id = ?;
+WHERE id = ?;
--- db/user.sql
+++ db/user.sql
@@ -1,26 +1,25 @@
 -- thêm tài khoản (đăng ký/ tạo tài khoản trong trang admin)
-INSERT users (user_name,password,role_id,avatar,email,phone,address) 
+INSERT users (name,password,role_id,avatar,email,phone,address) 
 VALUES (?,?,?,?,?,?,?);
 
 -- sửa 1 tài khoản (update thông tin người dùng bao gồm: tên, số điện thoại, địa chỉ, email)
 UPDATE users SET
-user_name = ?
+name = ?
 password = ?
-role_id = ?
 avatar = ?
 email = ?
 phone = ?
 address = ?
-WHERE user_id = ?
+WHERE id = ?
 
 -- xóa 1 tài khoản (chức năng trang admin)
-DELETE FROM users WHERE user_id = ?;
+DELETE FROM users WHERE id = ?;
 
 -- lấy ra 1 tài khoản có user_id = ? (để làm chức năng đăng nhập)
-SELECT * FROM users WHERE user_id = ?;
+SELECT * FROM users WHERE id = ?;
 
 -- update vai trò của tài khoản (chức năng phân quyền của admin)
-UPDATE user_role SET role_name = ? WHERE role_id = ?;
+UPDATE user_role SET name = ? WHERE id = ?;
 
 -- 
 
