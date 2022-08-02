ae0e4ed41729303ec8e9e1761b439fbd45e246bd
quanghiep031 <quanghiep03198@gmail.com>
2022-08-02 23:59:31 +0700

    update validate from server side by php

--- lib/validate.php
+++ lib/validate.php
@@ -5,86 +5,82 @@
  * Định nghĩa ra các rule check validate
  * -----------------------------------------
  */
-function check_empty($input_name, $error_name)
+function check_empty()
 {
-    global $error;
-    if (isset($_POST[$input_name]) && empty($_POST[$input_name])) :
-        $error[$input_name] = "Bạn chưa nhập " . $error_name;
-        echo   $error[$input_name];
-    endif;
+    global $error_count;
+    $form_controls = func_get_args();
+    foreach ($form_controls as $form_ctrl)
+        if (isset($form_ctrl) && empty($form_ctrl))
+            $error_count++;
 }
 
-function check_email($input_name)
+function check_email($form_ctrl)
 {
-    global $error;
+    global $error_count;
     $regex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
     if (
-        isset($_POST[$input_name]) &&
-        !empty($_POST[$input_name])  &&
-        !preg_match($regex, $_POST[$input_name])
-    ) :
-        $error[$input_name] = "Bạn chưa nhập email";
-        echo   $error[$input_name];
-    endif;
+        isset($form_ctrl) &&
+        !empty($form_ctrl)  &&
+        !preg_match($regex, $form_ctrl)
+    )
+        $error_count++;
+    // echo   $error_count;
+
 }
 // check giá trị trùng khớp
-function check_matching($input_name1, $input_name2, $error_name)
+function check_matching($form_ctrl1, $form_ctrl2)
 {
-    global $error;
+    global $error_count;
     if (
-        isset($_POST[$input_name2]) &&
-        !empty($_POST[$input_name2]) &&
-        $_POST[$input_name1] != $_POST[$input_name2]
-    ) :
-        $error[$input_name2] = $error_name . " không trùng khớp";
-        echo $error[$input_name2];
-    endif;
+        isset($_POST[$form_ctrl1]) &&
+        !empty($_POST[$form_ctrl2]) &&
+        $form_ctrl1 != $form_ctrl2
+    )
+        $error_count++;
 }
 // check độ dài
-function check_length($input_name, $minLength, $error_name)
+function check_length($form_ctrl, $minLength)
 {
-    global $error;
+    global $error_count;
     if (
-        isset($_POST[$input_name]) &&
-        !empty($_POST[$input_name]) &&
-        strlen($_POST[$input_name]) < $minLength
+        isset($form_ctrl) &&
+        !empty($form_ctrl) &&
+        strlen($form_ctrl) < $minLength
     ) :
-        $error[$input_name] = ucfirst($error_name) . "  phải có ít nhất {$minLength} ký tự";
-        echo   $error[$input_name];
+        $error_count++;
+
+    // echo   $error_count;
     endif;
 }
 
 // check số điện thoại
-function check_phoneNumber($input_name)
+function check_phoneNumber($form_ctrl)
 {
-    global $error;
+    global $error_count;
     if (
-        isset($_POST[$input_name]) &&
-        !empty($_POST[$input_name]) &&
-        strlen($_POST[$input_name]) != 10 &&
-        !is_numeric($_POST[$input_name])
+        isset($form_ctrl) &&
+        !empty($form_ctrl) &&
+        strlen($form_ctrl) != 10 &&
+        !is_numeric($form_ctrl)
     ) :
-        $error[$input_name] =  "Số điện thoại không hợp lệ !";
-        echo   $error[$input_name];
+        $error_count++;
+    // echo   $error_count;
     endif;
 }
 // check file ảnh
-function check_image($input_name, $submitType)
+function check_image($name_attr, $submitType)
 {
-    global $error;
+    global $error_count;
     $allowedFile = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'jfif'];
-    if (isset($_POST[$submitType]) && empty($_FILES[$input_name]['name'])) :
-        $error[$input_name] = "Bạn chưa upload ảnh";
-        echo   $error[$input_name];
+    if (isset($_POST[$submitType]) && empty($_FILES[$name_attr]['name'])) :
+        $error_count++;
     endif;
-    if (isset($_POST[$submitType]) && !empty($_FILES[$input_name]['name'])) :
-        $file_ext = explode(".", $_FILES[$input_name]['name']);
+    if (isset($_POST[$submitType]) && !empty($_FILES[$name_attr]['name'])) :
+        $file_ext = explode(".", $_FILES[$name_attr]['name']);
         if (is_array($file_ext)) :
             $isImageFile = in_array($file_ext[1], $allowedFile);
-
             if (!$isImageFile) :
-                $error[$input_name] = "File không phải là ảnh!";
-                echo  $error[$input_name];
+                $error_count++;
             endif;
         endif;
     endif;
