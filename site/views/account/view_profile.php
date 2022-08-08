c653d3782773a79ccb6785a67b9c5203d67e3cca
quanghiep031 <quanghiep03198@gmail.com>
2022-08-08 12:17:42 +0700

    fixed: hiển thị bootstrap icon

--- site/views/account/view_profile.php
+++ site/views/account/view_profile.php
@@ -53,7 +53,7 @@
     <!-- import footer component -->
     <?php include_once "site/components/footer.php" ?>
     <!--  -->
-    <script src="/js/handle-userdata.js"></script>
+    <script src="/js/common.js"></script>
 </body>
 
 </html>
