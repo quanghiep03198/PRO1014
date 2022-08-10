5980eeebc9dffd8cb644d4aad196fe605aa81b46
quanghiep031 <quanghiep03198@gmail.com>
2022-08-11 02:30:24 +0700

    fixed: areRequired

--- js/handle-userdata.js
+++ js/handle-userdata.js
@@ -3,7 +3,7 @@ const handleErrorChangePassword = (form, event) => {
 	const curPassword = form["current_password"];
 	const newPassword = form["new_password"];
 	const cfmPassword = form["cfm_new_password"];
-	if (!isRequired(curPassword, newPassword, cfmPassword)) event.preventDefault();
+	if (!areRequired(curPassword, newPassword, cfmPassword)) event.preventDefault();
 	if (isMatchingValue(newPassword, cfmPassword) == false) event.preventDefault();
 	if (checkLength(newPassword, 8) == false) event.preventDefault();
 	return true;
@@ -15,7 +15,7 @@ const handleErrorUpdateProfile = (form, event) => {
 	const phone = form["phone"];
 	console.log(email);
 	const address = form["address"];
-	if (isRequired(userName, email, phone, address) === false) event.preventDefault();
+	if (areRequired(userName, email, phone, address) === false) event.preventDefault();
 	if (isEmail(email) === false) event.preventDefault();
 	if (isPhoneNumber(phone) === false) event.preventDefault();
 };
@@ -29,20 +29,71 @@ const handleRegisterError = (form, event) => {
 	const email = form["email"];
 	const address = form["address"];
 	const phone = form["phone"];
-	if (!isRequired(account, password, confirmPassword, username, email, address, phone)) event.preventDefault();
-	if (checkLength(password, 8) == false) event.preventDefault();
-	if (ckMatchingValue(confirmPassword, password) == false) event.preventDefault();
-	if (isEmail(email) == false) event.preventDefault();
-	if (isPhoneNumber(phone) == false) event.preventDefault();
+	let isSuccess = true;
+	if (areRequired(account, password, confirmPassword, username, email, address, phone) == false) {
+		event.preventDefault();
+		isSuccess = false;
+	}
+	if (checkLength(password, 8) == false) {
+		event.preventDefault();
+		isSuccess = false;
+	}
+	if (ckMatchingValue(confirmPassword, password) == false) {
+		event.preventDefault();
+		isSuccess = false;
+	}
+	if (isEmail(email) == false) {
+		event.preventDefault();
+		isSuccess = false;
+	}
+	if (isPhoneNumber(phone) == false) {
+		event.preventDefault();
+		isSuccess = false;
+	}
+	if (isSuccess)
+		swal({
+			title: "Check email để nhận mã xác thực!",
+			icon: "info",
+			button: true,
+		});
 };
 // validate form tra cứu đơn hàng
-const handleErrorWarrantySearch = (form, event) => {
+
+const handleErrorWarrantySearch = async (button) => {
+	const form = button.parentElement;
 	const customer_infor = form["customer_infor"];
 	const order_key_id = form["order_key_id"];
-	if (!isRequired(customer_infor, order_key_id)) event.preventDefault();
-	if (!isEmail(customer_infor)) event.preventDefault();
+	let isSuccess = true;
+	if (areRequired(customer_infor, order_key_id) == false) isSuccess = false;
+	if (isEmail(customer_infor) == false) isSuccess = false;
+	// nếu validate thông tin ng dùng nhập hợp lệ -> check xem thông tin đơn hàng ng dùng nhập có tồn tại
+	if (isSuccess) {
+		button.setAttribute("for", "warranty-info");
+		const response = await sendRequest("/site/controllers/handle_warranty.php", {
+			customer_infor: customer_infor.value,
+			order_key_id: order_key_id.value,
+		});
+		const result = JSON.parse(response);
+		const orderItemsList = $("#order-items-list");
+		orderItemsList.innerHTML = result
+			.map(
+				(item) => /*html */ `
+				<div class="flex items-center gap-3">
+					<img src="/img/products/${item.image}" alt="" class="w-24 h-24 object-cover">
+					<div class="flex flex-col justify-center gap-2">
+						<span class="font-semibold">${item.product_name}</span>
+						<span class="font-semibold">${item.unit_price}</span>
+						<span class="font-normal">Ngày tạo đơn: ${item.create_date}</span>
+						<span class="font-normal">Bảo hành đến: ${item.warranty_expire_date}</span>
+						</div>
+				</div>`,
+			)
+			.join("");
+		console.log(result);
+	}
 };
-
+// nguyenngochoang2121998@gmail.com
+// f5f96025
 const loadFile = (event) => {
 	const photo = $("#user-image");
 	photo.style.display = "block";
@@ -53,14 +104,43 @@ const handleGetVerifyCode = (form, event) => {
 	const account = form["account"];
 	const email = form["email"];
 	console.log(email);
-	if (!isRequired(account, email)) event.preventDefault();
-	if (!isEmail(email)) event.preventDefault();
+	if (areRequired(account, email) == false) event.preventDefault();
+	if (isEmail(email) == false) event.preventDefault();
 };
 const handleResetPassword = (form) => {};
 
-const handleLoginError = (form, event) => {
-	event.preventDefault()
-	const account = form['account'];
-	const password = form['password']
-	sendRequest("")
+const handleLoginError = async (form, event) => {
+	event.preventDefault();
+	const account = form["account"];
+	const password = form["password"];
+	const saveAccountCheckbox = form["save-account__checkbox"];
+	areRequired(account, password);
+	const response = await sendRequest("/site/controllers/handle_login.php", {
+		account: account.value,
+		password: password.value,
+	});
+	console.log(JSON.parse(response));
+	const resUserData = JSON.parse(response);
+	if (resUserData.hasOwnProperty("err_account")) await showError(account, resUserData.err_account);
+	if (resUserData.hasOwnProperty("err_password")) await showError(password, resUserData.err_password);
+	if (resUserData.hasOwnProperty("account"))
+		swal({
+			title: "Đăng nhập thành công!",
+			icon: "success",
+			button: false,
+			timer: 1500,
+		})
+			.then(() => {
+				document.cookie = `auth=${resUserData.id}`;
+				if (saveAccountCheckbox.checked == true) localStorage.setItem("account", resUserData.id.toString());
+			})
+			.then(() => {
+				if (resUserData.role_id == 3) window.location = window.location.href;
+				else window.location = "admin.php";
+			});
+};
+const logout = () => {
+	document.cookie = "auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+	localStorage.clear();
+	sessionStorage.clear();
 };
